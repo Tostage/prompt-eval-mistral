999a94e094c68bc70e434acb56ce6458dec50007
Tostage <cristofmorissette1@gmail.com>
2025-06-03 08:41:42 -0600

    Create test_prompt.py

---
+++ test_prompt.py
@@ -0,0 +1,6 @@
+from llm_runner import generate_response
+
+prompt = "Explain the difference between a planet and a star."
+response = generate_response(prompt)
+print("\n=== RESPONSE ===\n")
+print(response)
